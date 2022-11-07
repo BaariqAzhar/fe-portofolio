4ee39a6937314b048e20b84ae0f7bf929bdbf96e
baariq <baariq.fairuuz@dataon.com>
2022-11-07 14:18:56 +0700

    create axios env

--- src/components/global/layout/Navbar.js
+++ src/components/global/layout/Navbar.js
@@ -1,4 +1,22 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { API_NODE_EXPRESS } from '../../../helper/constants';
+
+const url = `${API_NODE_EXPRESS}intro`;
+
 const Navbar = () => {
+    const [menu, setMenu] = useState([]);
+
+    useEffect(() => {
+        axios.get(url).then((response) => {
+            setMenu(response.data);
+        });
+    }, []);
+
+    useEffect(() => {
+        console.log(menu);
+    }, [menu]);
+
     return (
         <>
             <h1>This is Navbar</h1>
---
+++ src/helper/constants.js
@@ -0,0 +1,1 @@
+export const API_NODE_EXPRESS = process.env.REACT_APP_NODE_EXPRESS;
